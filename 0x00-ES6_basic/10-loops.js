c865f2ed828d6933002223c74057e20887b86718
Nellymaina <99280364+Nellymaina@users.noreply.github.com>
2022-12-21 00:15:50 +0300

    Mandatory tasks

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,11 @@
+export default function appendToEachArrayValue(array, appendString) {
+  // eslint-disable-next-line
+	for (const value of array) {
+    // eslint-disable-next-line
+		array[array.indexOf(value)] = appendString + value;
+  }
+
+  return array;
+}
+Footer
+© 2022 Git
