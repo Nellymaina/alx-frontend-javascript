c63b816a1af73f97627d24e69de47ce693b9357b
Nellymaina <99280364+Nellymaina@users.noreply.github.com>
2023-01-03 20:30:07 +0300

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,8 +1,5 @@
-export default function getStudentIdsSum(getListStudents) {
-  const myArr = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-  return myArr.reduce(((reducer, myArr) => reducer + myArr.id), 0);
+export default function getStudentIdsSum(lst) {
+  return lst.reduce((counter, currentObj) => counter + currentObj.id, 0);
+}export default function getStudentIdsSum(lst) {
+  return lst.reduce((counter, currentObj) => counter + currentObj.id, 0);
 }
