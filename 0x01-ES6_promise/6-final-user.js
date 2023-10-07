b38955bf78b9d533dec95a7b0b6326a72be7fc21
Nellymaina <99280364+Nellymaina@users.noreply.github.com>
2023-10-07 11:52:58 +0300

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from "./4-user-promise.js"
 import uploadPhoto from "./5-photo-reject.js"
  export default function handleProfileSignup(firstName, lastName, fileName){
 
-Promise.allSettled([signUpuser, uploadPhoto]).then((value)=>{
+return Promise.allSettled([signUpuser, uploadPhoto]).then((value)=>{
 console.log(value)});
  }
    
