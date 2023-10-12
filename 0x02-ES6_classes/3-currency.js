34f9eb8836901460d9636863b3998c32f365b82d
Nellymaina <99280364+Nellymaina@users.noreply.github.com>
2023-10-12 16:51:08 +0300

    Update 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,7 +1,7 @@
 export default class Currency {
   constructor(code, name) {
-    this.code = code;
-    this.name = name;
+    this._code = code;
+    this._name = name;
   }
 
   // code
@@ -10,7 +10,7 @@ export default class Currency {
   }
 
   set code(value) {
-    this._code = value;
+    return this._code = value;
   }
 
   // name
