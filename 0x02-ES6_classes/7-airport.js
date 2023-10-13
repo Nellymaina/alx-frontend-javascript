789f81eb8337c2e7f35ac5f274b6d520dceaa16a
Nellymaina <99280364+Nellymaina@users.noreply.github.com>
2023-10-13 15:27:24 +0300

    Update 7-airport.js

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -5,6 +5,6 @@ export default class Airport {
   }
 }
 
-Airport.prototype.toString = function airportToString() {
+Airport.prototype.toString = function airportTiString() {
   return `[object ${this._code}]`;
 };
